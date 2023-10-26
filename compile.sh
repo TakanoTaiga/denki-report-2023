0c693a2bd8b5e88b15ecf59bcafc5eb8af3409f6
Taiga Takano <ttttghghnb554z@outlook.jp>
2023-10-26 11:11:00 +0900

    Fix sh
    Signed-off-by: Taiga Takano <ttttghghnb554z@outlook.jp>

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
-tectonic ./example/graph.tex --outdir ./build
-tectonic ./example/report_temp.tex --outdir ./build
 tectonic ./report/3.再生可能エネルギーの実験.tex --outdir ./build
-
+# tectonic ./example/graph.tex --outdir ./build
+# tectonic ./example/report_temp.tex --outdir ./build
