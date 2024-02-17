affb7ae0fec4c00f0edff89bd63e7b0d13b0f727
Taiga Takano <ttttghghnb554z@outlook.jp>
2024-02-17 15:24:11 +0900

    Update

--- compile.sh
+++ compile.sh
@@ -12,6 +12,7 @@ tectonic ./report/2.直流機の実験.tex --outdir ./build
 tectonic ./report/3.再生可能エネルギーの実験.tex --outdir ./build
 tectonic ./report/4.電界効果トランジスタの実験.tex --outdir ./build
 tectonic ./report/5.線形回路の過渡現象.tex --outdir ./build
+tectonic ./report/6.増幅回路の実験.tex --outdir ./build
 
 tectonic ./example/graph.tex --outdir ./build
 tectonic ./example/report_temp.tex --outdir ./build
